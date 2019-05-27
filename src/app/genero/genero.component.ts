b6aba51d17a9591acc221b50feff0103efc1ead2
Lucas Pedro <lucaspedrolbg@gmail.com>
2019-05-27 04:11:55 -0300

    apresentado as informações de genero no component gereno e config router

--- src/app/genero/genero.component.ts
+++ src/app/genero/genero.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { DiscoService } from '../disco.service';
 
 @Component({
   selector: 'app-genero',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./genero.component.css']
 })
 export class GeneroComponent implements OnInit {
-
-  constructor() { }
+  genero = null
+  constructor(private disco: DiscoService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.genero = this.disco.encontrarGenero(parseInt(id));
   }
 
 }
